2c3381f92f7e85ec5c0e0aed57f9975fa0585678
Sergei Zinin <zinin@xakep.ru>
2014-04-01 15:37:37 +0800

    Expenses specification

---
+++ lib/mavenlink/expense.rb
@@ -0,0 +1,4 @@
+module Mavenlink
+  class Expense < Model
+  end
+end
---
+++ spec/lib/mavenlink/expense_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Mavenlink::Expense, stub_requests: true do
+  it_should_behave_like 'model', 'expenses'
+
+  describe 'validations' do
+    it { should validate_presence_of :workspace_id }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :category }
+    it { should validate_presence_of :amount_in_cents }
+  end
+
+  describe 'associations' do
+    it { should respond_to :workspace }
+    it { should respond_to :user }
+    it { should respond_to :receipt }
+  end
+end
