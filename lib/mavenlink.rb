7bd2a32c364507549b81d353a21f2fea014c754b
Integrations Dev <solutions+integrations@mavenlink.com>
2016-02-03 14:22:16 -0800

    Add workspace invoice preferences

--- lib/mavenlink.rb
+++ lib/mavenlink.rb
@@ -108,5 +108,6 @@ require 'mavenlink/resolution'
 require 'mavenlink/role' # NOTE(SZ): remove
 require 'mavenlink/user'
 require 'mavenlink/workspace'
+require 'mavenlink/workspace_invoice_preference'
 require 'mavenlink/workspace_group'
 require 'mavenlink/railtie' if defined?(Rails)
