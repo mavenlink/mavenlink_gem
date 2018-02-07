fbfbe016e90fabbf88e44e5a3cee38cdf319b9f1
Kevin Cotugno <kcotugno@mavenlink.com>
2018-02-07 15:16:19 -0800

    Spec

--- spec/lib/mavenlink/workspace_spec.rb
+++ spec/lib/mavenlink/workspace_spec.rb
@@ -6,6 +6,8 @@ describe Mavenlink::Workspace, stub_requests: true do
   it { should be_a Mavenlink::Model }
   it { should be_a Mavenlink::Concerns::Indestructible }
 
+  it { should respond_to :external_references }
+
   let(:response) {
     {
       'count' => 1,
