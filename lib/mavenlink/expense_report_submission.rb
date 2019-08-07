794e88080b270b871b8d37ee15b1a9e9e2b33780
Kevin Cotugno <kcotugno@mavenlink.com>
2019-08-07 14:30:11 -0700

    Add support for approving expense reports

--- lib/mavenlink/expense_report_submission.rb
+++ lib/mavenlink/expense_report_submission.rb
@@ -1,5 +1,9 @@
 module Mavenlink
   class ExpenseReportSubmission < Model
+    def approve_submission
+      client.put("expense_report_submissions/#{self.id}/approve")
+    end
+
     def reject_submission
       client.put("expense_report_submissions/#{self.id}/reject")
     end
--- spec/lib/mavenlink/expense_report_submission_spec.rb
+++ spec/lib/mavenlink/expense_report_submission_spec.rb
@@ -3,6 +3,17 @@ require 'spec_helper'
 describe Mavenlink::ExpenseReportSubmission, stub_requests: true do
   let(:report) { described_class.new(id: 4) }
 
+  describe "#approve_submission" do
+    before do
+      allow(report.client).to receive(:put)
+      report.approve_submission
+    end
+
+    it "uses a put method with the correct api endpoint and id" do
+      expect(report.client).to have_received(:put).with("expense_report_submissions/4/approve")
+    end
+  end
+
   describe "#reject_submission" do
     before do
       allow(report.client).to receive(:put)
