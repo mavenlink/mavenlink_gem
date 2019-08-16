e0b8cced0c5626c4de54d486bdc1f6b36ea21ae6
Kevin Cotugno <kcotugno@mavenlink.com>
2019-08-16 11:12:17 -0700

    Add timesheet approve and reject methods

--- lib/mavenlink/timesheet_submission.rb
+++ lib/mavenlink/timesheet_submission.rb
@@ -1,4 +1,11 @@
 module Mavenlink
   class TimesheetSubmission < Model
+    def approve_submission
+      client.put("timesheet_submissions/#{self.id}/approve")
+    end
+
+    def reject_submission
+      client.put("timesheet_submissions/#{self.id}/reject")
+    end
   end
 end
