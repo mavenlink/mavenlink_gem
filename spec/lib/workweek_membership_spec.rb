ab195879704062f8316f1b9cb2da9a2afe124eeb
Kevin Cotugno <kcotugno@mavenlink.com>
2019-10-30 09:26:56 -0700

    Add shared behaviors to WorkweekMembership

--- spec/lib/workweek_membership_spec.rb
+++ spec/lib/workweek_membership_spec.rb
@@ -1,6 +1,10 @@
 require "spec_helper"
 
-describe Mavenlink::WorkweekMembership do
+describe Mavenlink::WorkweekMembership, stub_requests: true, type: :model do
+  it_should_behave_like "model", "workweek_memberships"
+
+  it { is_expected.to be_a Mavenlink::Model }
+
   describe "associations" do
     it { is_expected.to respond_to :user }
     it { is_expected.to respond_to :workweek }
