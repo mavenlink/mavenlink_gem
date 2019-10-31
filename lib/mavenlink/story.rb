7743ceccf39ac0ea4a745c03d5b3fe9b1c366575
Kevin Cotugno <kcotugno@mavenlink.com>
2019-10-31 12:06:47 -0700

    Add story association load filters

--- lib/mavenlink/story.rb
+++ lib/mavenlink/story.rb
@@ -2,5 +2,13 @@ module Mavenlink
   class Story < Model
     validates :title, :story_type, :workspace_id, presence: true
     validates :story_type, inclusion: { in: %w[task deliverable milestone issue] }
+
+    def association_load_filters
+      {
+        show_archived: true,
+        show_deleted: true,
+        show_from_archived_workspaces: true
+      }
+    end
   end
 end
