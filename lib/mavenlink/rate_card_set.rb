16586419708723a8935fb9ce050a8effd15a1cf1
Luke Walsh <lwalsh@mavenlink.com>
2020-01-29 14:20:09 -0800

    Add #clone and #clone! method to rate card set model
    Co-authored-by: Daniel Rassiner <drassiner@mavenlink.com>

--- lib/mavenlink/rate_card_set.rb
+++ lib/mavenlink/rate_card_set.rb
@@ -1,4 +1,24 @@
 module Mavenlink
   class RateCardSet < Model
+    def clone!(set_version_id, title: nil, include: nil)
+      raise Mavenlink::Error.new("Record not defined") unless persisted?
+
+      request.perform do
+        client.post(
+          collection_name,
+          clone_version_id: set_version_id,
+          include: include,
+          rate_card_set: {
+            title: title,
+          }
+        )
+      end.results.first
+    end
+
+    def clone(set_version_id, title: nil, include: nil)
+      clone!(set_version_id, title: title, include: include)
+    rescue Mavenlink::Error
+      false
+    end
   end
 end
