0c1670f5d0fdebb825f8def0f2d4ba8dce4b26f6
Kevin Cotugno <kcotugno@mavenlink.com>
2020-02-20 12:32:53 -0800

    Remove invalid account_membership association

--- spec/lib/mavenlink/backup_approver_association_spec.rb
+++ spec/lib/mavenlink/backup_approver_association_spec.rb
@@ -4,7 +4,6 @@ describe Mavenlink::BackupApproverAssociation, stub_requests: true, type: :model
   it_should_behave_like "model", "backup_approver_associations"
 
   describe "association" do
-    it { is_expected.to respond_to :account_membership }
     it { is_expected.to respond_to :approver }
     it { is_expected.to respond_to :backup_approver }
   end
