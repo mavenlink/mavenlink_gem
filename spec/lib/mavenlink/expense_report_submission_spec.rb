b7e37a8f1003f74b8a3fb1caf367b3ad62e1a3d6
Luke Walsh <lwalsh@mavenlink.com>
2020-06-16 11:50:15 -0700

    Add external reference association to expense_report_submissions
    Co-authored-by: John Flores <jflores@mavenlink.com>

--- spec/lib/mavenlink/expense_report_submission_spec.rb
+++ spec/lib/mavenlink/expense_report_submission_spec.rb
@@ -2,4 +2,12 @@ require "spec_helper"
 
 describe Mavenlink::ExpenseReportSubmission, stub_requests: true, type: :model do
   it_behaves_like Mavenlink::Submission
+
+  describe "associations" do
+    it { is_expected.to respond_to :user }
+    it { is_expected.to respond_to :workspace }
+    it { is_expected.to respond_to :expenses }
+    it { is_expected.to respond_to :resolutions }
+    it { is_expected.to respond_to :external_references }
+  end
 end
