fe9f350b3a4e9cdfd291cbe5dcfb928f3c398b79
Matthew Deol <mdeol@mavenlink.com>
2020-07-28 16:03:52 -0700

    Add Project Accounting Records to the Mavenlink Gem
    Co-authored-by: Luke Walsh <lwalsh@mavenlink.com>

---
+++ lib/mavenlink/project_accounting_record.rb
@@ -0,0 +1,4 @@
+module Mavenlink
+  class ProjectAccountingRecord < Model
+  end
+end
---
+++ spec/lib/mavenlink/project_accounting_record_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe Mavenlink::ProjectAccountingRecord, stub_requests: true, type: :model do
+  it_should_behave_like "model", "project_accounting_records"
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of :workspace_id }
+    it { is_expected.to validate_presence_of :amount }
+    it { is_expected.to validate_presence_of :currency }
+    it { is_expected.to validate_presence_of :start_date }
+    it { is_expected.to validate_presence_of :end_date }
+    it { is_expected.to validate_presence_of :type }
+  end
+
+  describe "associations" do
+    it { is_expected.to respond_to :story }
+    it { is_expected.to respond_to :workspace }
+  end
+end
