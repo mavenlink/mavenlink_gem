d2bad3622a701f16b4fa675df5461aa8b05b625e
simeon13 <33508230+simeon13@users.noreply.github.com>
2021-07-15 15:17:49 -0700

    remove association for follower

--- spec/lib/mavenlink/story_follower_spec.rb
+++ spec/lib/mavenlink/story_follower_spec.rb
@@ -2,7 +2,6 @@ require "spec_helper"
 
 describe Mavenlink::StoryFollower, type: :model do
   describe "associations" do
-    it { is_expected.to respond_to :follower }
     it { is_expected.to respond_to :story }
   end
 end
