07402f936bca0307826ab38d5d31372f5bc19b4c
simeon13 <33508230+simeon13@users.noreply.github.com>
2021-07-15 15:14:57 -0700

    fix associations

--- spec/lib/mavenlink/story_follower_spec.rb
+++ spec/lib/mavenlink/story_follower_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Mavenlink::StoryFollower, type: :model do
   describe "associations" do
-    it { is_expected.to respond_to :user }
+    it { is_expected.to respond_to :follower }
     it { is_expected.to respond_to :story }
   end
 end
