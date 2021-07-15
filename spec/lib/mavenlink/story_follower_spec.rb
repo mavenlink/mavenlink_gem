7dd61bafa47b01551219bbb45f7ae17c8c913f84
simeon13 <33508230+simeon13@users.noreply.github.com>
2021-07-15 15:11:07 -0700

    remove stub requests from spec

--- spec/lib/mavenlink/story_follower_spec.rb
+++ spec/lib/mavenlink/story_follower_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 
-describe Mavenlink::StoryFollower, stub_requests: true, type: :model do
-  it_should_behave_like "model", "story_followers"
-
+describe Mavenlink::StoryFollower, type: :model do
   describe "associations" do
     it { is_expected.to respond_to :user }
     it { is_expected.to respond_to :story }
