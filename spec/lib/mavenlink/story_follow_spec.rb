9958ab7c5e534bd4fb4114d88c21e6b0a2bf004f
Matthew Deol <mdeol@mavenlink.com>
2021-07-21 15:53:35 -0600

    fix followers to follower
    add shared behavior for model
    
    Co-authored-by: Simeon Lam <slam@mavenlink.com>

--- spec/lib/mavenlink/story_follow_spec.rb
+++ spec/lib/mavenlink/story_follow_spec.rb
@@ -1,7 +1,10 @@
 require "spec_helper"
 
-describe Mavenlink::StoryFollow, type: :model do
+describe Mavenlink::StoryFollow, stub_requests: true, type: :model do
+  it_should_behave_like "model", "story_follows"
+
   describe "associations" do
     it { is_expected.to respond_to :story }
+    it { is_expected.to respond_to :follower }
   end
 end
