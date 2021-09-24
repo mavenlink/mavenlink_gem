1b7f3e8713a46dea304df0616a3054b46dbb6a40
John Flores <jflores@mavenlink.com>
2021-09-24 12:40:01 -0700

    Add ExchangeTable#rates
    Co-authored-by: Chan Sy <chan.song.sy@gmail.com>
    Co-authored-by: Simeon Lam <slam@mavenlink.com>

--- lib/mavenlink/exchange_table.rb
+++ lib/mavenlink/exchange_table.rb
@@ -3,5 +3,18 @@ module Mavenlink
     def self.collection_path(_ = {})
       "foreign_exchange/exchange_tables"
     end
+
+    # NOTE: The API response is currently bugged. It returns results with an incorrect key
+    #   "exchange_rate" should be plural "exchange_rates" to satisfy brainstem requirements.
+    #
+    #   To get around this bug we need to manually initialize the exchange rate objects returned.
+    #   Ideally we should be able to just call `results` on the response and have this be handles automatically.
+    def rates(filters = {})
+      response = request.perform { client.get("#{self.class.collection_path}/#{id}/rates", filters) }
+
+      response.response_data["exchange_rates"].values.map do |exchange_rate_attributes|
+        Mavenlink::ExchangeRate.new(exchange_rate_attributes, nil, client)
+      end
+    end
   end
 end
