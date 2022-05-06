720ae9005ad75cd76dea395d58882a912a9e5bba
Simeon Lam <slam@mavenlink.com>
2022-05-06 12:16:34 -0700

    Add Split Endpoint Call to Workspace Allocations

--- lib/mavenlink/workspace_allocation.rb
+++ lib/mavenlink/workspace_allocation.rb
@@ -1,4 +1,7 @@
 module Mavenlink
   class WorkspaceAllocation < Model
+    def split_allocation(date)
+      client.put("#{collection_name}/split", split_date: date, workspace_allocation_id: id)
+    end
   end
 end
--- spec/lib/mavenlink/workspace_allocation_spec.rb
+++ spec/lib/mavenlink/workspace_allocation_spec.rb
@@ -17,4 +17,14 @@ describe Mavenlink::WorkspaceAllocation, stub_requests: true, type: :model do
     it { is_expected.to respond_to "updater" }
     it { is_expected.to respond_to "external_references" }
   end
+
+  describe "#split_allocation" do
+    subject { described_class.new(id: "5555") }
+    let(:date) { Date.today.to_s }
+
+    it "puts to the split route with the record id and date" do
+      expect(subject.client).to receive(:put).with("workspace_allocations/split", split_date: date, workspace_allocation_id: subject.id)
+      subject.split_allocation(date)
+    end
+  end
 end
