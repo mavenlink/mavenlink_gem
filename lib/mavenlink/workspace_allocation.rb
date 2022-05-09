9a33aa5d6ed0322e0090b56260d0ff9d9ebf2048
Simeon Lam <slam@mavenlink.com>
2022-05-09 14:14:58 -0700

    testing

--- lib/mavenlink/workspace_allocation.rb
+++ lib/mavenlink/workspace_allocation.rb
@@ -1,8 +1,8 @@
 module Mavenlink
   class WorkspaceAllocation < Model
     def split_allocation(date)
-      client.put("#{collection_name}/split", split_date: date, workspace_allocation_id: id)
       client.put("#{collection_name}/#{id}", end_date: date.to_date.yesterday.to_s)
+      client.put("#{collection_name}/split", split_date: date, workspace_allocation_id: id)
     end
   end
 end
