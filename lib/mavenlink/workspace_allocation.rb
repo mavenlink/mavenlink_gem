4fa9e5bc4bdf80bd3a971d1e3d4e397771bdf23b
Simeon Lam <slam@mavenlink.com>
2022-05-09 15:02:33 -0700

    add create workspace allocation

--- lib/mavenlink/workspace_allocation.rb
+++ lib/mavenlink/workspace_allocation.rb
@@ -1,7 +1,12 @@
 module Mavenlink
   class WorkspaceAllocation < Model
     def split_allocation(date)
-      client.put("#{collection_name}/split", split_date: date, workspace_allocation_id: id)
+      response = client.put("#{collection_name}/split", split_date: date, workspace_allocation_id: id)
+      ## update
+      # update_attributes(response.response_data["workspace_allocations"].values.first)
+
+      ## create
+      Mavenlink::WorkspaceAllocation.new(response["workspace_allocations"].values.last, nil, client)
     end
   end
 end
