9073537d6a658d47d4a08d4a7a24af8360a02512
Simeon Lam <slam@mavenlink.com>
2022-05-09 13:40:06 -0700

    update the calling on allocation's attributes

--- spec/lib/mavenlink/workspace_allocation_spec.rb
+++ spec/lib/mavenlink/workspace_allocation_spec.rb
@@ -23,7 +23,8 @@ describe Mavenlink::WorkspaceAllocation, stub_requests: true, type: :model do
     let(:date) { Date.today.to_s }
 
     it "puts to the split route with the record id and date" do
-      expect(subject.client).to receive(:put).with("workspace_allocations/split", split_date: date, workspace_allocation_id: subject.id)
+      expect(subject.client).to receive(:put).with("workspace_allocations/split", split_date: date.to_date.tomorrow.to_s, workspace_allocation_id: subject.id)
+      expect(subject.client).to receive(:put).with("workspace_allocations/5555", end_date: date)
       subject.split_allocation(date)
     end
   end
