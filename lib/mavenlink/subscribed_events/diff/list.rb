fe5d99737a6e58c7f2e2fcbd0ea71515184f2a67
Matthew Deol <mdeol@mavenlink.com>
2022-05-19 13:32:41 -0600

    lint and add verification of payload

--- lib/mavenlink/subscribed_events/diff/list.rb
+++ lib/mavenlink/subscribed_events/diff/list.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 module Mavenlink
   module SubscribedEvents
     class Diff
       class List
+        MISSING_FIELDS_MESSAGE = "Subscribed events must include optional fields `previous_payload` and `current_payload`"
+
         def initialize(events)
           @events = events
 
           # Validate events include optional fields for `current_payload,previous_payload` like in diff
+          raise ArgumentError, MISSING_FIELDS_MESSAGE if missing_required_fields?
         end
 
         def grouped_diffs
@@ -17,19 +22,23 @@ module Mavenlink
         attr_reader :events
 
         def subject_diffs
-          subject_events.group_by do |event|
-            [event.subject_type, event.subject_id]
-          end.each_with_object([]) do |(key, grouped_events), array|
+          grouped_subject_events.each_with_object([]) do |(_key, grouped_events), array|
             array << Mavenlink::SubscribedEvents::Diff.from_collection(grouped_events)
           end
         end
 
         def subject_custom_field_diffs
-          subject_custom_field_events.group_by do |event|
-            [event.subject_type, event.subject_id, event.current_payload[:custom_field_id]]
-          end.each_with_object([]) do |(key, grouped_events), array|
+          grouped_subject_custom_field_events.each_with_object([]) do |(_key, grouped_events), array|
             array << Mavenlink::SubscribedEvents::Diff.from_collection(grouped_events)
           end
+        end
+
+        def grouped_subject_events
+          subject_events.group_by { |event| [event.subject_type, event.subject_id] }
+        end
+
+        def grouped_subject_custom_field_events
+          subject_custom_field_events.group_by { |event| [event.subject_type, event.subject_id, event.current_payload[:custom_field_id]] }
         end
 
         def subject_events
@@ -42,6 +51,10 @@ module Mavenlink
 
         def custom_field_event?(event_type)
           event_type.include?(":custom_field_value_")
+        end
+
+        def missing_required_fields?
+          events.any? { |event| !event.key?(:previous_payload) || !event.key?(:current_payload) }
         end
       end
     end
