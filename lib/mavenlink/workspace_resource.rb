e4121290981849dbb23ad97063c30b1680f07024
Simeon Lam <slam@mavenlink.com>
2022-05-19 17:39:54 -0700

    add workspace id as a parameter

--- lib/mavenlink/workspace_resource.rb
+++ lib/mavenlink/workspace_resource.rb
@@ -1,7 +1,7 @@
 module Mavenlink
   class WorkspaceResource < Model
-    def allocations_matching_scheduled_hours
-      client.post("#{collection_name}/allocations_matching_scheduled_hours")
+    def allocations_matching_scheduled_hours(workspace_id)
+      client.post("#{collection_name}/allocations_matching_scheduled_hours", workspace_id: workspace_id)
     end
   end
 end
