6b801270f591c1e60af10f90457ef544d10ae7af
Simeon Lam <slam@mavenlink.com>
2022-05-19 17:16:13 -0700

    add post call for allocations matching scheduled hours

--- lib/mavenlink/workspace_resource.rb
+++ lib/mavenlink/workspace_resource.rb
@@ -1,4 +1,7 @@
 module Mavenlink
   class WorkspaceResource < Model
+    def allocations_matching_scheduled_hours
+      client.post("#{collection_name}/allocations_matching_scheduled_hours")
+    end
   end
 end
--- spec/lib/mavenlink/workspace_resource_spec.rb
+++ spec/lib/mavenlink/workspace_resource_spec.rb
@@ -14,4 +14,17 @@ describe Mavenlink::WorkspaceResource, stub_requests: true, type: :model do
     it { is_expected.to respond_to "skills" }
     it { is_expected.to respond_to "workspace" }
   end
+
+  describe "#allocations_matching_scheduled_hours" do
+    subject { described_class.new(id: 4) }
+
+    before do
+      allow(subject.client).to receive(:post)
+    end
+
+    it "posts to the allocations_matching_scheduled_hours route" do
+      expect(subject.client).to receive(:post).with("#{described_class.collection_name}/allocations_matching_scheduled_hours")
+      subject.allocations_matching_scheduled_hours
+    end
+  end
 end
