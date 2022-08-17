c7f46d8db68a0e54cad00a708d62ee35d205558f
John Flores <jflores@mavenlink.com>
2022-08-17 13:52:06 -0700

    Add external references to roles

--- spec/lib/mavenlink/role_spec.rb
+++ spec/lib/mavenlink/role_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 describe Mavenlink::Role, stub_requests: true, type: :model do
   it_should_behave_like "model", "roles"
 
+  describe "associations" do
+    it { is_expected.to respond_to :external_references }
+  end
+
   describe "validations" do
     it { is_expected.to validate_presence_of :name }
   end
